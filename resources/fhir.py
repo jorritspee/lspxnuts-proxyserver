dcbf394b186e8a91d1208b643bf832eec8e07019
jorritspee <jorrit@jorritspee.nl>
2023-05-24 10:05:42 +0200

    added Fhir-resource: WIP!

---
+++ resources/fhir.py
@@ -0,0 +1,32 @@
+from flask import jsonify, request, current_app
+from flask_restful import Resource
+
+class Fhir(Resource):
+  
+    # corresponds to the GET request.
+    # this function is called whenever there
+    # is a GET request for this resource
+    def get(self):
+        return {
+                "hello": "world",
+                "you": "are doing great! keep up the good work",
+                "config": {
+                    "base url nuts node": current_app.config["BASE_URL_NUTS_NODE"],
+                    "base url lsp": current_app.config["BASE_URL_LSP"],
+                    "lsp application id": current_app.config["LSP_APPLICATION_ID"],
+                    "filename transation token": current_app.config["FILENAME_TRANSACTION_TOKEN"]
+                },
+                "resources": ["/", "/request-access-token", "/notify"]
+            }
+        #return {'task': 'Hello world'}
+
+        #return make_response(jsonify({'message': 'hello world'}), 200)
+  
+    # Corresponds to POST request
+    def post(self):
+
+        data = request.get_json()     # status code
+        #return jsonify(1,2,3), 201
+        return data, 201
+        #return jsonify(data), 201
+        #return jsonify({'data': data}), 201
