0498d903e59abb4df492b7d14f120c9d4000a970
Marvin Siliakus <134294366+msiliakus@users.noreply.github.com>
2023-05-24 23:48:16 +0200

    Notification Transformation

--- aorta/aorta_notification_transformer.py
+++ aorta/aorta_notification_transformer.py
@@ -2,419 +2,173 @@ from flask import current_app
 import json
 
 class Aorta_notification_transformer:
-    def transform_notification(task_ta_np_json):
+    def transform_notification(source_task):
         """2.16 transform notification"""
-        task_aof = json.dumps({
-      "resourceType": "Task",
-      "id": "nl-vzvz-TaskNotifiedPull-request-example",
-      "meta": {
-        "profile": [
-          "http://vzvz.nl/fhir/StructureDefinition/nl-vzvz-TaskNotifiedPull"
-        ]
-      },
-      "contained": [
-        {
-          "resourceType": "Device",
-          "id": "device1",
-          "meta": {
-            "profile": [
-              "http://vzvz.nl/fhir/StructureDefinition/nl-vzvz-Device"
-            ]
-          },
-          "identifier": [
-            {
-              "system": "http://fhir.nl/fhir/NamingSystem/aorta-app-id",
-              "value": "110"
+        target_task_code = {}
+        target_task_for = {}
+        target_task_device = {}
+        target_task_owner = {}
+        target_task_restriction = {}
+        target_task_input = []
+        # --------
+        # - code -
+        # --------
+        try:
+            #source_task_code_coding_system = source_task['code']['coding'][0]['system']
+            #source_task_code_coding_code = source_task['code']['coding'][0]['code']
+            target_task_code = {
+                "coding": [
+                    {
+                        "system": "http://vzvz.nl/fhir/CodeSystem/aorta-taskcode",
+                        "code": "notified_pull"
+                    }
+                ]
             }
-          ],
-          "deviceName": [
-            {
-              "name": "TestApplicatie",
-              "type": "user-friendly-name"
+        except:
+            print('Error mapping code')
+        # -------
+        # - for -
+        # -------
+        try:
+            source_task_for_identifier_system = source_task['for']['identifier']['system']
+            source_task_for_identifier_value = source_task['for']['identifier']['value']
+            target_task_for = {
+                "identifier": {
+                    "system": f"{source_task_for_identifier_system}",
+                    "value": f"{source_task_for_identifier_value}"
+                }
             }
-          ],
-          "owner": {
-            "identifier": {
-              "system": "http://fhir.nl/fhir/NamingSystem/ura",
-              "value": "90000380"
+        except:
+            print('Error mapping for')
+        # ----------------------
+        # - contained (Device) -
+        # ----------------------
+        try:
+            source_task_requester_agent_identifier_system = source_task['requester']['agent']['identifier']['system']
+            source_task_requester_agent_identifier_value = source_task['requester']['agent']['identifier']['value']
+            source_task_requester_onbehalfof_identifier_system = source_task['requester']['onBehalfOf']['identifier']['system']
+            source_task_requester_onbehalfof_identifier_value = source_task['requester']['onBehalfOf']['identifier']['value']
+            target_task_device = {
+                "resourceType": "Device",
+                "id": "device1",
+                "meta": {
+                    "profile": [
+                        "http://vzvz.nl/fhir/StructureDefinition/nl-vzvz-Device"
+                    ]
+                },
+                "identifier": [
+                    {
+                        "system": f"{source_task_requester_agent_identifier_system}",
+                        "value": f"{source_task_requester_agent_identifier_value}"
+                    }
+                ],
+                "deviceName": [
+                    {
+                        "name": "TestApplicatie",
+                        "type": "user-friendly-name"
+                    }
+                ],
+                "owner": {
+                    "identifier": {
+                        "system": f"{source_task_requester_onbehalfof_identifier_system}",
+                        "value": f"{source_task_requester_onbehalfof_identifier_value}"
+                    }
+                }
             }
-          }
+        except:
+            print('Error mapping contained (Device)')
+        # ---------
+        # - owner -
+        # ---------
+        try:
+            source_task_owner_identifier_system = source_task['owner']['identifier']['system']
+            source_task_owner_identifier_value = source_task['owner']['identifier']['value']
+            target_task_owner = {
+                "identifier": {
+                    "system": f"{source_task_owner_identifier_system}",
+                    "value": f"{source_task_owner_identifier_value}"
+                }
+            }
+        except:
+            print('Error mapping owner')        
+        # ---------------
+        # - restriction -
+        # ---------------
+        try:
+            source_task_restriction_period_end = source_task['restriction']['period']['end']
+            target_task_restriction = {
+                "period": {
+                    "end": f"{source_task_restriction_period_end}"
+                }
+            }
+        except:
+            print('Error mapping restriction') 
+        # ---------
+        # - input -
+        # ---------
+        try:
+            target_task_input = [];
+            for source_task_input_item in source_task['input']:
+                if source_task_input_item['type']['coding'][0]['code'] == 'authorization_base':
+                    # ----------------------
+                    # - authorization_base -
+                    # ----------------------
+                    source_task_input_item_valueString = source_task_input_item['valueString']
+                    target_task_input_item = {
+                        "type": {
+                            "coding": [
+                                {
+                                    "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
+                                    "code": "consent_token"
+                                }
+                            ]
+                        },
+                        "valueString": f"{source_task_input_item_valueString}"
+                    }
+                    target_task_input.append(target_task_input_item)
+                elif source_task_input_item['type']['coding'][0]['code'] == 'query_resource':
+                    # ------------------
+                    # - query_resource -
+                    # ------------------
+                    source_task_input_item_valueString = source_task_input_item['valueString']
+                    target_task_input_item = {
+                        "type": {
+                            "coding": [
+                                {
+                                    "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
+                                    "code": "query_string"
+                                }
+                            ]
+                        },
+                        "valueString": f"{source_task_input_item_valueString}"
+                    }
+                    target_task_input.append(target_task_input_item)
+        except:
+            print('Error mapping input')
+        # --------
+        # - Task -
+        # --------
+        target_task = {
+            "resourceType": "Task",
+            "id": "nl-vzvz-TaskNotifiedPull-request-example",
+            "meta": {
+                "profile": [
+                    "http://vzvz.nl/fhir/StructureDefinition/nl-vzvz-TaskNotifiedPull"
+                    ]
+                },
+            "contained": [
+                target_task_device
+            ],
+            "status": "requested",
+            "intent": "proposal",
+            "code": target_task_code,
+            "for": target_task_for,
+            "requester": {
+                "reference": "#device1",
+                "type": "Device"
+            },
+            "owner": target_task_owner,
+            "restriction": target_task_restriction,
+            "input": target_task_input
         }
-      ],
-      "status": "requested",
-      "intent": "proposal",
-      "code": {
-        "coding": [
-          {
-            "system": "http://vzvz.nl/fhir/CodeSystem/aorta-taskcode",
-            "code": "notified_pull"
-          }
-        ]
-      },
-      "for": {
-        "identifier": {
-          "system": "http://fhir.nl/fhir/NamingSystem/bsn",
-          "value": "012345672"
-        }
-      },
-      "requester": {
-        "reference": "#device1",
-        "type": "Device"
-      },
-      "owner": {
-        "identifier": {
-          "system": "http://fhir.nl/fhir/NamingSystem/ura",
-          "value": "90000380"
-        }
-      },
-      "restriction": {
-        "period": {
-          "end": "2023-05-05T12:00:00+02:00"
-        }
-      },
-      "input": [
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "consent_token"
-              }
-            ]
-          },
-          "valueString": "did:nuts:6AiKV8hQx5nMaSfLNByaxiBjN3XBEUx4LmiHh2iYwuYe"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Patient?_include=Patient:general-practitioner"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Coverage?_include=Coverage:payor:Patient&_include=Coverage:payor:Organization"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Consent?category=http://snomed.info/sct|11291000146105"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Consent?category=http://snomed.info/sct|11341000146107"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation/$lastn?category=http://snomed.info/sct|118228005,http://snomed.info/sct|384821006"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Condition"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation/$lastn?code=http://snomed.info/sct|365508006"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation?code=http://snomed.info/sct|228366006"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation?code=http://snomed.info/sct|228273003"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation?code=http://snomed.info/sct|365980008"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "NutritionOrder"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Flag"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "AllergyIntolerance"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "MedicationStatement?category=urn:oid:2.16.840.1.113883.2.4.3.11.60.20.77.5.3|6&_include=MedicationStatement:medication"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "MedicationRequest?category=http://snomed.info/sct|16076005&_include=MedicationRequest:medication"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "MedicationDispense?category=http://snomed.info/sct|422037009&_include=MedicationDispense:medication"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "DeviceUseStatement?_include=DeviceUseStatement:device"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Immunization?status=completed"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation/$lastn?code=http://loinc.org|85354-9"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation/$lastn?code=http://loinc.org|29463-7"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation/$lastn?code=http://loinc.org|8302-2,http://loinc.org|8306-3,http://loinc.org|8308-9"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Observation/$lastn?category=http://snomed.info/sct|275711006&_include=Observation:related-target&_include=Observation:specimen"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Procedure?category=http://snomed.info/sct|387713003"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Encounter?class=http://hl7.org/fhir/v3/ActCode|IMP,http://hl7.org/fhir/v3/ActCode|ACUTE,http://hl7.org/fhir/v3/ActCode|NONAC"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "ProcedureRequest?status=active"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "ImmunizationRecommendation"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "MedicationDispense?category=http://snomed.info/sct|422037009&status=in-progress,preparation&_include=MedicationDispense:medication"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "DeviceRequest?status=active&_include=DeviceRequest:device"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "Appointment?status=booked,pending,proposed"
-        },
-        {
-          "type": {
-            "coding": [
-              {
-                "system": "http://vzvz.nl/fhir/CodeSystem/TaskParameterType",
-                "code": "query_string"
-              }
-            ]
-          },
-          "valueString": "DocumentReference?status=current&indexed=ge2022-01-01"
-        }
-      ]
-    })
-        return task_aof
-    
+        return json.dumps(target_task)
