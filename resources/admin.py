fe88760370edd889e1493ed2891e53df9728a3ad
stevenvegt <steven.vandervegt@nedap.com>
2023-05-24 13:18:22 +0000

    Fix issuer

--- resources/admin.py
+++ resources/admin.py
@@ -4,40 +4,35 @@ from nuts.nuts_vcr_client import Nuts_vcr_client
 import requests
 
 class Admin_Issue_UraCredential(Resource):
-  
+
     def get(self):
         return {"you": "did a get"}
-    
+
     def post(self):
         try:
             data = request.get_json()
-            subject_did = data.subject
-            subject_ura = data.ura
-        except:
+            subject_did = data['subject']
+            subject_ura = data['ura']
+        except Exception as e:
             return {
-                        "invalid request body, use":
-                        {
-                            "subject": "<did:nuts of subject>",
-                            "ura": "urn:oid: 2.16.528.1.1007.3.3.<ura of subject>"
-                        }
-                    }, 406
+                "invalid request body, use":
+                {
+                    "subject": "<did:nuts of subject>",
+                    "ura": "urn:oid: 2.16.528.1.1007.3.3.<ura of subject>"
+                }
+            }, 406
 
         endpoint = current_app.config["BASE_URL_NUTS_NODE"] + "/internal/vcr/v2/issuer/vc"
         requestObj = {
-                        "@context": [
-                            "https://www.w3.org/2018/credentials/v1",
-                            "https://nuts.nl/credentials/v1"
-                        ],
-                        "type": [
-                            "VerifiableCredential",
-                            "VzvzUraCredential"
-                        ],
+                        "@context": "https://nuts.nl/credentials/v1",
+                        "type": "VzvzUraCredential",
                         "issuer": current_app.config['VENDOR_DID'],
-                        "visibility": "public",
                         "credentialSubject": {
                             "id": subject_did,
                             "nuts:ura": "urn:oid: 2.16.528.1.1007.3.3." + subject_ura
-                        }
+                        },
+                        "visibility": "public",
+                        "publishToNetwork": True
                     }
 
         response = requests.post(endpoint, json=requestObj)
